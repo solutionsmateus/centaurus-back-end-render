cde309a8b08f8cfa06ab2b8daab31f5f6e2c083b
Pedro <pedro.gomes@grupomateus.com>
2025-06-20 09:38:33 -0300

    Render application

---
+++ app.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from flask import Flask, render_template, request, jsonify
+import subprocess
+
+
+app = Flask(__name__)
+
+
+LOJA_SCRIPT_MAP = {
+    "Assaí": "centaurus/back-end/models/date_paths/assai.py",
+    "Atacadão": "centaurus/back-end/models/date_paths/atacadão.py",
+    "Cometa Supermercados": "centaurus/back-end/models/date_paths/cometa.py",
+    "Frangolândia Supermercados": "centaurus/back-end/models/date_paths/frangolandia.py",
+    "Novo Atacarejo": "centaurus/back-end/models/date_paths/novoatacarejo.py",
+    "GBarbosa (Grupo Cencosud)": "centaurus/back-end/models/date_paths/gbarbosa.py"
+}
+
+
+@app.route('/executar_script', methods=['POST'])
+def executar_script():
+    data = request.get_json()
+    loja = data.get('loja')
+
+    script_path = LOJA_SCRIPT_MAP.get(loja)
+
+    if not script_path:
+        return jsonify({"message": f"Loja '{loja}' não mapeada para nenhum script."}), 400
+
+    try:
+        result = subprocess.run(
+            ["python", script_path, loja],
+            capture_output=True,
+            text=True
+        )
+
+        if result.returncode != 0:
+            return jsonify({"message": f"Erro ao executar o script: {result.stderr}"}), 500
+
+        return jsonify({"message": f"Script '{loja}' executado com sucesso para o loja '{loja}'."})
+    except Exception as e:
+        return jsonify({"message": f"Erro inesperado: {str(e)}"}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
