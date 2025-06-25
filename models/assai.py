ff5451520c8979d4abcb74e12ee43c738f0c8b24
Angelo Batista Pereira <angelo.pereira@grupomateus.com>
2025-06-25 08:52:43 -0300

    all

--- models/assai.py
+++ models/assai.py
@@ -16,17 +16,15 @@ import json # Importar json para saída formatada
 
 def get_chrome_driver():
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless") 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.binary_location = "/usr/bin/chromium-browser" 
-    
-    service = Service("/usr/bin/chromedriver")
+
+    service = Service(ChromeDriverManager().install()) 
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
-    
 
 
 driver = get_chrome_driver()
