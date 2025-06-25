6d25c7b19aa31088f80b98155efaa5f7172c6314
Angelo Batista Pereira <angelo.pereira@grupomateus.com>
2025-06-25 09:55:25 -0300

    Add chromium

--- models/assai.py
+++ models/assai.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chromium import ChromiumDriverManager
 from datetime import datetime
 import re
 import sys
@@ -20,7 +21,7 @@ def get_chrome_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    service = Service(ChromeDriverManager().install()) 
+    service = Service(ChromiumDriverManager().install())
     
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
