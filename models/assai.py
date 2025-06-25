452fd79404ff051ccc87045961a350e21bfb229b
Angelo Batista Pereira <angelo.pereira@grupomateus.com>
2025-06-25 10:16:39 -0300

    ...

--- models/assai.py
+++ models/assai.py
@@ -22,6 +22,7 @@ def get_chrome_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.binary_location = "/usr/bin/chromium-browser" 
     service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
     
     driver = webdriver.Chrome(service=service, options=chrome_options)
